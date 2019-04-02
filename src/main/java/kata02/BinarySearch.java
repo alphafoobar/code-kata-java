4c3217070109b707b24fa99efe33f1ea17da8af4
James Little <james@ergo.kiwi>
2019-04-03 00:40:52 +1300

    Added java implementation for kata02

--- src/main/java/kata02/BinarySearch.java
+++ src/main/java/kata02/BinarySearch.java
@@ -1,8 +1,27 @@
 package kata02;
 
+import static java.util.Arrays.copyOfRange;
+
 public class BinarySearch {
 
-    public int chop(int key, int[] a) {
-        throw new UnsupportedOperationException("not implemented yet");
+    public int chop(int key, int[] array) {
+        return chop(key, array, 0);
+    }
+
+    public int chop(int key, int[] array, int offset) {
+        if (array.length == 0) {
+            return -1;
+        }
+        if (array.length == 1) {
+            return array[0] == key ? offset : -1;
+        }
+        int midPoint = Math.min(array.length - 1, array.length / 2);
+        if (array[midPoint] == key) {
+            return offset + midPoint;
+        }
+        if (array[midPoint] > key) {
+            return chop(key, copyOfRange(array, 0, midPoint), offset);
+        }
+        return chop(key, copyOfRange(array, midPoint + 1, array.length), offset + midPoint + 1);
     }
 }
--- src/test/java/kata02/BinarySearchTest.java
+++ src/test/java/kata02/BinarySearchTest.java
@@ -1,6 +1,6 @@
 package kata02;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,36 +10,43 @@ class BinarySearchTest {
     private BinarySearch search = new BinarySearch();
 
     @Test
-    @DisplayName("Easy tests")
-    void easyScenarios() {
-        assertEquals(-1, search.chop(3, new int[]{}));
-        assertEquals(-1, search.chop(3, new int[]{1}));
-        assertEquals(0, search.chop(1, new int[]{1}));
+    @DisplayName("Cannot find key in empty set")
+    void emptyScenarios() {
+        assertThat(search.chop(3, new int[]{})).isEqualTo(-1);
     }
 
     @Test
-    @DisplayName("Tests of 3 items")
+    @DisplayName("Single item array")
+    void easyScenarios() {
+        int[] a = {1};
+
+        assertThat(search.chop(3, a)).isEqualTo(-1);
+
+        assertThat(search.chop(1, a)).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("Tests of 3")
     void smallSets() {
-        assertEquals(0, search.chop(1, new int[]{1, 3, 5}));
-        assertEquals(1, search.chop(3, new int[]{1, 3, 5}));
-        assertEquals(2, search.chop(5, new int[]{1, 3, 5}));
-        assertEquals(-1, search.chop(0, new int[]{1, 3, 5}));
-        assertEquals(-1, search.chop(2, new int[]{1, 3, 5}));
-        assertEquals(-1, search.chop(4, new int[]{1, 3, 5}));
-        assertEquals(-1, search.chop(6, new int[]{1, 3, 5}));
+        int[] ints = {1, 3, 5};
+
+        assertThat(search.chop(1, ints)).isEqualTo(0);
+        assertThat(search.chop(3, ints)).isEqualTo(1);
+        assertThat(search.chop(5, ints)).isEqualTo(2);
+
+        assertThat(search.chop(0, ints)).isEqualTo(-1);
+        assertThat(search.chop(2, ints)).isEqualTo(-1);
+        assertThat(search.chop(4, ints)).isEqualTo(-1);
+        assertThat(search.chop(6, ints)).isEqualTo(-1);
     }
 
     @Test
     @DisplayName("Tests of 4")
     void mediumSets() {
-        assertEquals(0, search.chop(1, new int[]{1, 3, 5, 7}));
-        assertEquals(1, search.chop(3, new int[]{1, 3, 5, 7}));
-        assertEquals(2, search.chop(5, new int[]{1, 3, 5, 7}));
-        assertEquals(3, search.chop(7, new int[]{1, 3, 5, 7}));
-        assertEquals(-1, search.chop(0, new int[]{1, 3, 5, 7}));
-        assertEquals(-1, search.chop(2, new int[]{1, 3, 5, 7}));
-        assertEquals(-1, search.chop(4, new int[]{1, 3, 5, 7}));
-        assertEquals(-1, search.chop(6, new int[]{1, 3, 5, 7}));
-        assertEquals(-1, search.chop(8, new int[]{1, 3, 5, 7}));
+        int[] ints = {1, 3, 5, 7};
+
+        assertThat(search.chop(7, ints)).isEqualTo(3);
+
+        assertThat(search.chop(8, ints)).isEqualTo(-1);
     }
 }
