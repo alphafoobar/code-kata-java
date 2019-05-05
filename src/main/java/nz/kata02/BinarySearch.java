4cb8b8a69148e93a501e105649d4fe5de11834a0
James Little <alphafoobar@users.noreply.github.com>
2019-05-05 22:37:35 +1200

    Avoid array copy function
    - Just pass through markers to the original array.

--- src/main/java/nz/kata02/BinarySearch.java
+++ src/main/java/nz/kata02/BinarySearch.java
@@ -1,28 +1,23 @@
 package nz.kata02;
 
-import static java.util.Arrays.copyOfRange;
-
 class BinarySearch {
 
     int chop(int key, int[] array) {
-        return chop(key, array, 0);
+        return chop(key, array, 0, array.length);
     }
 
-    private int chop(int key, int[] array, int offset) {
-        if (array.length == 0) {
+    private int chop(int key, int[] array, int offset, int length) {
+        int remaining = length - offset;
+        if (remaining == 0) {
             return -1;
         }
-        if (array.length == 1) {
-            return array[0] == key ? offset : -1;
+        int point = offset + remaining / 2;
+        if (array[point] == key) {
+            return point;
         }
-
-        int midPoint = Math.min(array.length - 1, array.length / 2);
-        if (array[midPoint] == key) {
-            return offset + midPoint;
+        if (array[point] > key) {
+            return chop(key, array, offset, point);
         }
-        if (array[midPoint] > key) {
-            return chop(key, copyOfRange(array, 0, midPoint), offset);
-        }
-        return chop(key, copyOfRange(array, midPoint + 1, array.length), offset + midPoint + 1);
+        return chop(key, array, point + 1, length);
     }
 }
